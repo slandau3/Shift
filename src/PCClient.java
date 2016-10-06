f6e063d908386403e69c1b5d960a1c1e326de0e7
Steven Landau <skl8881@g.rit.edu>
2016-10-06 13:51:01 -0400

    Initial commit

---
+++ src/PCClient.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Steven Landau on 10/6/2016.
+ */
+public class PCClient extends Application {
+
+
+    /**
+     * The constructor for the PCClient class.
+     * Sets up the necessary sockets and streams
+     * also starts GUI
+     * @param ip, the ip to connect to
+     */
+    public PCClient(String ip) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
