ed6bec6eebc599946183b855a73b2a185ae45f67
Steven Landau <skl8881@g.rit.edu>
2016-10-06 13:53:10 -0400

    Added various private objects

--- src/PCClient.java
+++ src/PCClient.java
@@ -1,11 +1,24 @@
 import javafx.application.Application;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
 /**
  * Created by Steven Landau on 10/6/2016.
  */
 public class PCClient extends Application {
 
+    private Socket server;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private TextField inputBar;
+    private TextArea messageDisplay;
+    private VBox chats;
 
     /**
      * The constructor for the PCClient class.
